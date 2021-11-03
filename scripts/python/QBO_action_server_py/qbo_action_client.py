57f31ce541e0f02f71794577e1063e95d29381b6
SRFG-MAT <mathias.schmoigl@salzburgresearch.at>
2021-11-03 10:06:33 +0100

    slots in client now part of function call

--- scripts/python/QBO_action_server_py/qbo_action_client.py
+++ scripts/python/QBO_action_server_py/qbo_action_client.py
@@ -14,91 +14,91 @@ import rxt_skills_qbo.msg
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
 # client request implementations of QBO action server functions
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
-def qbo_request_VoiceOutput():
+def qbo_request_VoiceOutput(msgBytes):
     
     rospy.init_node('qbo_client_py') # Initializes a rospy node so that the SimpleActionClient can publish and subscribe over ROS
 
     client = actionlib.SimpleActionClient('VoiceOutput', rxt_skills_qbo.msg.VoiceOutputAction) # Creates SimpleActionClient with VoiceOutputAction action type
     client.wait_for_server() # Waits until the action server has started up and started listening for goals
-    goal = rxt_skills_qbo.msg.VoiceOutputGoal(outputMessage=b'Hallo ich bin ein sozialer Roboter') # Creates a goal to send to the action server
+    goal = rxt_skills_qbo.msg.VoiceOutputGoal(outputMessage=msgBytes) # Creates a goal to send to the action server
     client.send_goal(goal) # Sends the goal to the action server
     client.wait_for_result() # Waits for the server to finish performing the action
     
     return client.get_result() # Prints out the result (VoiceOutputResult) of executing the action
 
 
-def qbo_request_WaitForUserInput():
+def qbo_request_WaitForUserInput(msgBytes):
     
     rospy.init_node('qbo_client_py') # Initializes a rospy node so that the SimpleActionClient can publish and subscribe over ROS
 
     client = actionlib.SimpleActionClient('WaitForUserInput', rxt_skills_qbo.msg.WaitForUserInputAction) # Creates SimpleActionClient with WaitForUserInputAction action type
     client.wait_for_server() # Waits until the action server has started up and started listening for goals
-    goal = rxt_skills_qbo.msg.WaitForUserInputGoal(inputContent=b'void') # Creates a goal to send to the action server
+    goal = rxt_skills_qbo.msg.WaitForUserInputGoal(inputContent=msgBytes) # Creates a goal to send to the action server
     client.send_goal(goal) # Sends the goal to the action server
     client.wait_for_result() # Waits for the server to finish performing the action
     
     return client.get_result() # Prints out the result (WaitForUserInputResult) of executing the action
 
 
-def qbo_request_MoveToLocation():
+def qbo_request_MoveToLocation(msgBytes):
     
     rospy.init_node('qbo_client_py') # Initializes a rospy node so that the SimpleActionClient can publish and subscribe over ROS
 
     client = actionlib.SimpleActionClient('MoveToLocation', rxt_skills_qbo.msg.MoveToLocationAction) # Creates SimpleActionClient with MoveToLocationAction action type
     client.wait_for_server() # Waits until the action server has started up and started listening for goals
-    goal = rxt_skills_qbo.msg.MoveToLocationGoal(location=b'right') # Creates a goal to send to the action server
+    goal = rxt_skills_qbo.msg.MoveToLocationGoal(location=msgBytes) # Creates a goal to send to the action server
     client.send_goal(goal) # Sends the goal to the action server
     client.wait_for_result() # Waits for the server to finish performing the action
     
     return client.get_result() # Prints out the result (MoveToLocationResult) of executing the action
     
     
-def qbo_request_WaitForExternalEvent():
+def qbo_request_WaitForExternalEvent(msgBytes):
     
     rospy.init_node('qbo_client_py') # Initializes a rospy node so that the SimpleActionClient can publish and subscribe over ROS
 
     client = actionlib.SimpleActionClient('WaitForExternalEvent', rxt_skills_qbo.msg.WaitForExternalEventAction) # Creates SimpleActionClient WaitForExternalEventAction action type
     client.wait_for_server() # Waits until the action server has started up and started listening for goals
-    goal = rxt_skills_qbo.msg.WaitForExternalEventGoal(inputText=b'fear') # Creates a goal to send to the action server
+    goal = rxt_skills_qbo.msg.WaitForExternalEventGoal(inputText=msgBytes) # Creates a goal to send to the action server
     client.send_goal(goal) # Sends the goal to the action server
     client.wait_for_result() # Waits for the server to finish performing the action
     
     return client.get_result() # Prints out the result (WaitForExternalEventResult) of executing the action
     
     
-def qbo_request_GraphicalUserInteraction():
+def qbo_request_GraphicalUserInteraction(msgBytes):
     
     rospy.init_node('qbo_client_py') # Initializes a rospy node so that the SimpleActionClient can publish and subscribe over ROS
 
     client = actionlib.SimpleActionClient('GraphicalUserInteraction', rxt_skills_qbo.msg.GraphicalUserInteractionAction) # Creates SimpleActionClient with GraphicalUserInteractionAction action type
     client.wait_for_server() # Waits until the action server has started up and started listening for goals
-    goal = rxt_skills_qbo.msg.GraphicalUserInteractionGoal(outputMessage=b'happy') # Creates a goal to send to the action server
+    goal = rxt_skills_qbo.msg.GraphicalUserInteractionGoal(outputMessage=msgBytes) # Creates a goal to send to the action server
     client.send_goal(goal) # Sends the goal to the action server
     client.wait_for_result() # Waits for the server to finish performing the action
     
     return client.get_result() # Prints out the result (GraphicalUserInteractionResult) of executing the action
     
     
-def qbo_request_GetData():
+def qbo_request_GetData(msgBytes):
     
     rospy.init_node('qbo_client_py') # Initializes a rospy node so that the SimpleActionClient can publish and subscribe over ROS
 
     client = actionlib.SimpleActionClient('GetData', rxt_skills_qbo.msg.GetDataAction) # Creates SimpleActionClient with GetDataAction action type
     client.wait_for_server() # Waits until the action server has started up and started listening for goals
-    goal = rxt_skills_qbo.msg.GetDataGoal(inputData=b'robotName') # Creates a goal to send to the action server
+    goal = rxt_skills_qbo.msg.GetDataGoal(inputData=msgBytes) # Creates a goal to send to the action server
     client.send_goal(goal) # Sends the goal to the action server
     client.wait_for_result() # Waits for the server to finish performing the action
     
     return client.get_result() # Prints out the result (GetDataResult) of executing the action
 
 
-def qbo_request_SetData():
+def qbo_request_SetData(msgBytes):
     
     rospy.init_node('qbo_client_py') # Initializes a rospy node so that the SimpleActionClient can publish and subscribe over ROS
 
     client = actionlib.SimpleActionClient('SetData', rxt_skills_qbo.msg.SetDataAction) # Creates SimpleActionClient with SetDataAction action type
     client.wait_for_server() # Waits until the action server has started up and started listening for goals
-    goal = rxt_skills_qbo.msg.SetDataGoal(outputData=b'Mario') # Creates a goal to send to the action server
+    goal = rxt_skills_qbo.msg.SetDataGoal(outputData=msgBytes) # Creates a goal to send to the action server
     client.send_goal(goal) # Sends the goal to the action server
     client.wait_for_result() # Waits for the server to finish performing the action
     
@@ -111,7 +111,7 @@ if __name__ == '__main__':
     try:	
         
         # request VoiceOutput
-        result = qbo_request_VoiceOutput()
+        result = qbo_request_VoiceOutput(b'Hallo ich bin ein sozialer Roboter')
         if result:
             print ('----------------------------------')
             print("Action was: VoiceOutput")
@@ -119,7 +119,7 @@ if __name__ == '__main__':
             print ('----------------------------------')
         
         # request WaitForUserInput
-        result = qbo_request_WaitForUserInput()
+        result = qbo_request_WaitForUserInput(b'void')
         if result:
             print ('----------------------------------')
             print("Action was: WaitForUserInput")
@@ -127,31 +127,31 @@ if __name__ == '__main__':
             print ('----------------------------------')
         
         # request MoveToLocation
-        result = qbo_request_MoveToLocation()
+        result = qbo_request_MoveToLocation(b'right')
         if result:
             print ('----------------------------------')
             print("Action was: MoveToLocation")
             print("Result was: " + str(result.isOK))
             print ('----------------------------------')
-        
-        # request WaitForExternalEvent
-        result = qbo_request_WaitForExternalEvent()
-        if result:
-            print ('----------------------------------')
-            print("Action was: WaitForExternalEvent")
-            print("Result was: " + str(result.isOK))
-            print ('----------------------------------')
-        
-        # request GraphicalUserInteraction
-        result = qbo_request_GraphicalUserInteraction()
+
+	# request GraphicalUserInteraction
+        result = qbo_request_GraphicalUserInteraction(b'happy')
         if result:
             print ('----------------------------------')
             print("Action was: GraphicalUserInteraction")
             print("Result was: " + str(result.isOK))
             print ('----------------------------------')
         
+        # request WaitForExternalEvent
+        result = qbo_request_WaitForExternalEvent(b'fear')
+        if result:
+            print ('----------------------------------')
+            print("Action was: WaitForExternalEvent")
+            print("Result was: " + str(result.isOK))
+            print ('----------------------------------')      
+        
         # request GetData
-        result = qbo_request_GetData()
+        result = qbo_request_GetData(b'robotName')
         if result:
             print ('----------------------------------')
             print("Action was: GetData")
@@ -159,7 +159,7 @@ if __name__ == '__main__':
             print ('----------------------------------')
         
         # request SetData
-        result = qbo_request_SetData()
+        result = qbo_request_SetData(b'Mario')
         if result:
             print ('----------------------------------')
             print("Action was: SetData")
