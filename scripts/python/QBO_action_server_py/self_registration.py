ed32b187c6f2cb0ee61668fa76633773d45a3331
SRFG-MAT <mathias.schmoigl@salzburgresearch.at>
2021-12-13 13:20:20 +0100

    added get/put/post to registration request

--- scripts/python/QBO_action_server_py/self_registration.py
+++ scripts/python/QBO_action_server_py/self_registration.py
@@ -25,7 +25,7 @@ def uploadAAS(aas):
     
     try:
         headers = {'Content-type': 'application/json'}    
-        r_get = requests.get(registration_endpoint + 'b8:27:eb:24:1f:b2', timeout=5, json=aas, headers=headers, auth=('devr', 'DevReg\!robXtask'))
+        r_get = requests.get(registration_endpoint + '/b8:27:eb:24:1f:b2', timeout=5, json=aas, headers=headers, auth=('devr', 'DevReg\!robXtask'))
 
         if r_get.ok:
             r_add = requests.put(registration_endpoint, timeout=5, json=aas, headers=headers, auth=('devr', 'DevReg\!robXtask')) 
